4d4b5b8df26c9064bd44881f9df5242a3e217f14
Tom Boutell <tom@apostrophecms.com>
2025-12-08 18:17:59 -0500

    busted 8a

---
+++ 8a.mjs
@@ -0,0 +1,53 @@
+import lines from './lib/lines.mjs';
+
+const option = process.argv.find(a => a.startsWith('--connections='));
+const connections = option ? parseInt(option.substring('--connections='.length), 10) : 1000;
+
+const boxes = lines({ split: ',', integers: true }).map(([ x, y, z], i) => ({ x, y, z, i, c: i }));
+const distances = [];
+
+for (let i = 0; (i < boxes.length); i++) {
+  for (let j = i + 1; (j < boxes.length); j++) {
+    const box = boxes[i];
+    const other = boxes[j];
+    const dx = other.x - box.x;
+    const dy = other.y - box.y;
+    const dz = other.z - box.z;
+    const d = Math.sqrt((dx * dx) + (dy * dy) + (dz * dz));
+    distances.push({
+      box,
+      other,
+      d
+    });
+  }
+}
+
+distances.sort((a, b) => {
+  return a.d - b.d;
+});
+
+for (let i = 0; (i < connections); i++) {
+  const { box, other } = distances[i];
+  if (box.c !== other.c) {
+    for (const b of boxes) {
+      if (b.c === other.c) {
+        b.c = box.c;
+      }
+    }
+  }
+}
+
+const circuits = new Map();
+for (const box of boxes) {
+  const circuit = circuits.get(box.c) || [];
+  circuit.push(box.i);
+  circuits.set(box.c, circuit);
+}
+
+const cIds = [...circuits.keys()];
+cIds.sort((a, b) => circuits.get(b).length - circuits.get(a).length);
+
+for (const id of cIds) {
+  console.log(circuits.get(id).length);
+}
+console.log(cIds.slice(0, 3).reduce((a, cId) => a * circuits.get(cId).length, 1));
